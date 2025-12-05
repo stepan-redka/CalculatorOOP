12f27333c891c2414a044296bccf706380961e04
stepan-redka <stepanredka17@gmail.com>
2025-12-05 14:43:20 +0200

    ops implementation

---
+++ ClassLibrary/Operations.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassLibrary
+{
+    public interface IOperation
+    {
+        public double DoOperation(double a, double b);
+    }
+
+    public class Addition : IOperation
+    {
+        public double DoOperation(double a, double b) =>  a + b;
+    }
+
+    public class Multiplication :  IOperation
+    {
+        public double DoOperation(double a, double b) =>  a * b;
+    }
+
+    public class Division :  IOperation
+    {
+        public double DoOperation(double a, double b) =>  a / b;
+    }
+
+    public class Subtraction :  IOperation
+    {
+        public double DoOperation(double a, double b) =>  a - b;
+    }
+}
+
